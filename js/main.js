db5316db71136fa4a562c0792c4537e6b9952a45
moonlight2028 <120291090+moonlight2028@users.noreply.github.com>
2023-05-31 20:25:39 -0500

    commit Figura

---
+++ js/main.js
@@ -0,0 +1,79 @@
+function circulo() {
+  let figura = document.getElementById("figura");
+  figura.classList = "";
+  figura.classList.toggle("circulo");
+}
+
+function rectangulo() {
+  let figura = document.getElementById("figura");
+  figura.classList = "";
+  figura.classList.toggle("rectangulo");
+}
+
+function triangulo() {
+  let figura = document.getElementById("figura");
+  figura.classList = "";
+  figura.classList.toggle("triangulo");
+}
+function girar() {
+  let figura = document.getElementById("figura");
+  figura.classList = "";
+  figura.classList.toggle("girar");
+}
+function arriba() {
+  let figura = document.getElementById("figura");
+  figura.classList = "";
+  figura.classList.toggle("arriba");
+}
+function abajo() {
+  let figura = document.getElementById("figura");
+  figura.classList = "";
+  figura.classList.toggle("abajo");
+}
+function izquierda() {
+  let figura = document.getElementById("figura");
+  figura.classList = "";
+  figura.classList.toggle("izquierda");
+}
+function derecha() {
+  let figura = document.getElementById("figura");
+  figura.classList = "";
+  figura.classList.toggle("derecha");
+}
+function oculto() {
+  let figura = document.getElementById("figura");
+  figura.classList = "";
+  figura.classList.toggle("oculto");
+}
+function gif() {
+  let figura = document.getElementById("figura");
+  figura.classList = "";
+  figura.classList.toggle("gif");
+}
+function degradado() {
+  let figura = document.getElementById("figura");
+  figura.classList = "";
+  figura.classList.toggle("degradado");
+}
+function spinner() {
+  let figura = document.getElementById("figura");
+  figura.classList = "";
+  figura.classList.toggle("spinner");
+}
+function loader() {
+  let figura = document.getElementById("figura");
+  figura.classList = "";
+  figura.classList.toggle("loader");
+}
+
+function panel() {
+  const panel = document.querySelector(".panelizq");
+  figura.classList = "";
+  panel.classList.toggle("activo");
+}
+
+function panelsup() {
+  const panel2 = document.querySelector(".panelsup");
+  figura.classList = "";
+  panel2.classList.toggle("activo2");
+}
